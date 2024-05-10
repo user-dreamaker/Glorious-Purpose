81767d7a293307c67fc40bbcdad10828f7f78d7c
Dreamaker <lj270889@hotmail.com>
2024-05-10 02:19:49 -0300

    Premier Ball Bonus System

--- include/strings.h
+++ include/strings.h
@@ -959,6 +959,8 @@ extern const u8 gText_InBagVar1[];
 extern const u8 gText_Var1AndYouWantedVar2[];
 extern const u8 gText_HereYouGoThankYou[];
 extern const u8 gText_NoMoreRoomForThis[];
+extern const u8 gText_ThrowInPremierBall[];
+extern const u8 gText_ThrowInPremierBalls[];
 
 // pokedex_screen
 extern const u8 gText_PokedexTableOfContents[];
