77e19241b95bbf0db18c2602461024b034129a48
Dreamaker <lj270889@hotmail.com>
2024-05-16 23:38:26 -0300

    New Game: PC Item

--- src/player_pc.c
+++ src/player_pc.c
@@ -98,7 +98,7 @@ static const struct MenuAction sMenuActions_ItemPc[] = {
 };
 
 static const struct ItemSlot gNewGamePCItems[] = {
-    { ITEM_POTION, 1 },
+    { ITEM_EXP_SHARE, 1 },
     { ITEM_NONE,   0 }
 };
 
