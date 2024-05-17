306897a6d43e8b5135d383f56f2929b7f8339355
Dreamaker <lj270889@hotmail.com>
2024-05-17 05:52:02 -0300

    Inheriting Natures (Everstone)
    If the mother (in a male-female pair) or Ditto (in a pair that includes Ditto) is holding an Everstone, the baby has a 50% chance of inheriting that Pokémon's Nature.

--- include/pokemon.h
+++ include/pokemon.h
@@ -371,6 +371,7 @@ bool8 PokemonItemUseNoEffect(struct Pokemon *mon, u16 item, u8 partyIndex, u8 mo
 u8 GetItemEffectParamOffset(u16 itemId, u8 effectByte, u8 effectBit);
 const u8 *Battle_PrintStatBoosterEffectMessage(u16 itemId);
 u8 GetNature(struct Pokemon *mon);
+u8 GetNatureFromPersonality(u32 personality);
 u16 GetEvolutionTargetSpecies(struct Pokemon *mon, u8 type, u16 evolutionItem);
 u16 NationalPokedexNumToSpecies(u16 nationalNum);
 u16 SpeciesToNationalPokedexNum(u16 species);
--- src/random.c
+++ src/random.c
@@ -5,6 +5,7 @@
 // of rand and srand in the ISO C standard.
 
 u32 gRngValue;
+u32 gRng2Value;
 
 u16 Random(void)
 {
@@ -15,4 +16,15 @@ u16 Random(void)
 void SeedRng(u16 seed)
 {
     gRngValue = seed;
+}
+
+u16 Random2(void)
+{
+    gRng2Value = ISO_RANDOMIZE1(gRng2Value);
+    return gRng2Value >> 16;
+}
+
+void SeedRng2(u16 seed)
+{
+    gRng2Value = seed;
 }
