3365a5d89cfd8ca610793bae130c8dae1b314ca0
Dreamaker <lj270889@hotmail.com>
2024-05-19 05:52:11 -0300

    Push B in Wild Battle to Run

--- src/battle_controller_player.c
+++ src/battle_controller_player.c
@@ -302,6 +302,16 @@ static void HandleInputChooseAction(void)
             BtlController_EmitTwoReturnValues(1, B_ACTION_CANCEL_PARTNER, 0);
             PlayerBufferExecCompleted();
         }
+        else
+        {
+            if(!(gBattleTypeFlags & BATTLE_TYPE_TRAINER))
+            {
+                PlaySE(SE_SELECT);
+                ActionSelectionDestroyCursorAt(gActionSelectionCursor[gActiveBattler]);
+                gActionSelectionCursor[gActiveBattler] = 3;
+                ActionSelectionCreateCursorAt(gActionSelectionCursor[gActiveBattler], 0);
+            }
+        }		
     }
     else if (JOY_NEW(START_BUTTON))
     {
