72f6c9b7799ac67699f4f53b5a2a4fd6fd3bbdf8
Dreamaker <lj270889@hotmail.com>
2024-05-23 13:19:12 -0300

    Move Update (Uproar)
    Now lasts 3 turns. (instead of 2-5 turns)

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -2509,7 +2509,7 @@ void SetMoveEffect(bool8 primary, u8 certain)
                 {
                     gBattleMons[gEffectBattler].status2 |= STATUS2_MULTIPLETURNS;
                     gLockedMoves[gEffectBattler] = gCurrentMove;
-                    gBattleMons[gEffectBattler].status2 |= STATUS2_UPROAR_TURN((Random() & 3) + 2); // 2-5 turns
+                    gBattleMons[gEffectBattler].status2 |= STATUS2_UPROAR_TURN(3); // 3 turns
 
                     BattleScriptPush(gBattlescriptCurrInstr + 1);
                     gBattlescriptCurrInstr = sMoveEffectBS_Ptrs[gBattleCommunication[MOVE_EFFECT_BYTE]];
