812126ec50f5b43d73ae94b77e24206df7362200
Dreamaker <lj270889@hotmail.com>
2024-05-24 15:02:58 -0300

    Ability Update (Effect Spore)
    There is a 1/3 chance that the attacking Pokémon will become either poisoned, paralyzed, or asleep. (instead 10%)

--- src/battle_util.c
+++ src/battle_util.c
@@ -2009,7 +2009,7 @@ u8 AbilityBattleEffects(u8 caseID, u8 battler, u8 ability, u8 special, u16 moveA
                  && !gProtectStructs[gBattlerAttacker].confusionSelfDmg
                  && TARGET_TURN_DAMAGED
                  && (gBattleMoves[move].flags & FLAG_MAKES_CONTACT)
-                 && (Random() % 10) == 0)
+                 && (Random() % 3) == 0)
                 {
                     do
                     {
