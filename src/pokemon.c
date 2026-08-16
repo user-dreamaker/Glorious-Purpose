cc18d899190c5f7277ecf4d0ae7d220cdf86b9f3
Dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-08-16 02:54:35 -0300

    Ability Update (Plus / Minus)
    When a Pokémon with Plus enters a battle and has an ally with Plus or Minus, the Special Attack stat of the Pokémon with Plus is increased by 50%.

--- src/pokemon.c
+++ src/pokemon.c
@@ -2519,9 +2519,9 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
         gBattleMovePower /= 2;
     if (attacker->ability == ABILITY_HUSTLE)
         attack = (150 * attack) / 100;
-    if (attacker->ability == ABILITY_PLUS && ABILITY_ON_FIELD2(ABILITY_MINUS))
+    if (attacker->ability == ABILITY_PLUS && (ABILITY_ON_FIELD2(ABILITY_MINUS) || ABILITY_ON_FIELD2(ABILITY_PLUS)))
         spAttack = (150 * spAttack) / 100;
-    if (attacker->ability == ABILITY_MINUS && ABILITY_ON_FIELD2(ABILITY_PLUS))
+    if (attacker->ability == ABILITY_MINUS && (ABILITY_ON_FIELD2(ABILITY_PLUS) || ABILITY_ON_FIELD2(ABILITY_MINUS)))
         spAttack = (150 * spAttack) / 100;
     if (attacker->ability == ABILITY_GUTS && attacker->status1)
         attack = (150 * attack) / 100;
