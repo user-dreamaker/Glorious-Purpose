ae4be8d38394ecf58aa792ccb4f2e7dffdf17c83
Dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-08-19 05:00:40 -0300

    Ability ADD (Magic Guard)
    Prevents indirect damage.

--- src/data/text/abilities.h
+++ src/data/text/abilities.h
@@ -92,6 +92,7 @@ static const u8 sHydrationDescription[] = _("Heals status problems in rain.");
 static const u8 sSolarPowerDescription[] = _("Trades HP for power in sunshine.");
 static const u8 sQuickFeetDescription[] = _("Ups SPEED if suffering.");
 static const u8 sSniperDescription[] = _("Boosts critical hits.");
+static const u8 sMagicGuardDescription[] = _("Prevents indirect damage.");
 
 const u8 *const gAbilityDescriptionPointers[ABILITIES_COUNT] =
 {
@@ -189,6 +190,7 @@ const u8 *const gAbilityDescriptionPointers[ABILITIES_COUNT] =
     [ABILITY_SOLAR_POWER] = sSolarPowerDescription,
     [ABILITY_QUICK_FEET] = sQuickFeetDescription,
     [ABILITY_SNIPER] = sSniperDescription,
+    [ABILITY_MAGIC_GUARD] = sMagicGuardDescription,
 };
 
 const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
@@ -287,4 +289,5 @@ const u8 gAbilityNames[ABILITIES_COUNT][ABILITY_NAME_LENGTH + 1] =
     [ABILITY_SOLAR_POWER] = _("SOLAR POWER"),
     [ABILITY_QUICK_FEET] = _("QUICK FEET"),
     [ABILITY_SNIPER] = _("SNIPER"),
+    [ABILITY_MAGIC_GUARD] = _("MAGIC GUARD"),
 };
