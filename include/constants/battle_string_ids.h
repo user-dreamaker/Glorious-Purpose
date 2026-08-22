707c0167bd91d45b4a08d60a89e0d6052fc86357
Dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-08-22 08:16:23 -0300

    Ability ADD (Snow Warning)
    Summons a hailstorm.

--- include/constants/battle_string_ids.h
+++ include/constants/battle_string_ids.h
@@ -387,8 +387,9 @@
 #define STRINGID_PKMNMAXEDATTACK 386
 #define STRINGID_PKMNHURTBYHARSHSUNLIGHT 387
 #define STRINGID_FOREWARNMADEAWARE 388
+#define STRINGID_PKMNSXWHIPPEDUPHAIL 389
 
-#define BATTLESTRINGS_COUNT     389
+#define BATTLESTRINGS_COUNT     390
 
 // This is the string id that gBattleStringsTable starts with.
 // String ids before this (e.g. STRINGID_INTROMSG) are not in the table,
