56e3999b3e7e6d1adf0189affd4a8aa0b850212e
Dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-08-22 08:47:18 -0300

    Ability ADD (Frisk)
    Reveals the foe's held item.

--- include/constants/battle.h
+++ include/constants/battle.h
@@ -153,6 +153,7 @@
 #define STATUS3_TRACE                   (1 << 20)
 #define STATUS3_DOWNLOAD                (1 << 21)
 #define STATUS3_FOREWARN                (1 << 22)
+#define STATUS3_FRISK                   (1 << 23)
 #define STATUS3_SEMI_INVULNERABLE       (STATUS3_UNDERGROUND | STATUS3_ON_AIR | STATUS3_UNDERWATER)
 
 // Not really sure what a "hitmarker" is.
