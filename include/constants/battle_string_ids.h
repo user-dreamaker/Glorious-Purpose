03f1d17097a4258a4a9e916e00964e7cb7d47fef
Dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-08-22 08:47:18 -0300

    Ability ADD (Frisk)
    Reveals the foe's held item.

--- include/constants/battle_string_ids.h
+++ include/constants/battle_string_ids.h
@@ -388,8 +388,9 @@
 #define STRINGID_PKMNHURTBYHARSHSUNLIGHT 387
 #define STRINGID_FOREWARNMADEAWARE 388
 #define STRINGID_PKMNSXWHIPPEDUPHAIL 389
+#define STRINGID_FRISK 390
 
-#define BATTLESTRINGS_COUNT     390
+#define BATTLESTRINGS_COUNT     391
 
 // This is the string id that gBattleStringsTable starts with.
 // String ids before this (e.g. STRINGID_INTROMSG) are not in the table,
