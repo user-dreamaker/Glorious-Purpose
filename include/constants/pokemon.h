430a94ff715c4367de1e2b6eee70548b9782ad58
user-dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-08-28 16:35:46 -0300

    Evolve by level up with a held item
    Evolution Method

--- include/constants/pokemon.h
+++ include/constants/pokemon.h
@@ -279,6 +279,8 @@
 #define EVO_LEVEL_NINJASK    13 // Pokémon reaches the specified level (special value for Ninjask)
 #define EVO_LEVEL_SHEDINJA   14 // Pokémon reaches the specified level (special value for Shedinja)
 #define EVO_BEAUTY           15 // Pokémon levels up with beauty ≥ specified value
+#define EVO_HELD_ITEM        16 // Pokémon levels up while holding specified item
+#define EVO_LEVEL_HELD_ITEM  16 // Pokémon levels up while holding specified item
 
 #define EVOS_PER_MON 5
 
