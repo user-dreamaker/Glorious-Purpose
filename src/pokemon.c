222b87baa9a7619b70f5e6e57052c5cdb806f31f
user-dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-08-28 16:04:37 -0300

    Disable Badge Stat Boost
    In Generations I-III, some badges boost certain stats of the player's Pokémon.
    This boost is not applied in link battles.
    In Generations I and II, the stat or stats are increased by 12.5%.
    In Generation III, the stat or stats are increased by 10%.
    This boost does not exist from Generation IV onward.

--- src/pokemon.c
+++ src/pokemon.c
@@ -2478,13 +2478,13 @@ s32 CalculateBaseDamage(struct BattlePokemon *attacker, struct BattlePokemon *de
         attack *= 2;
 
     if (ShouldGetStatBadgeBoost(FLAG_BADGE01_GET, battlerIdAtk))
-        attack = (110 * attack) / 100;
+        attack = (100 * attack) / 100;
     if (ShouldGetStatBadgeBoost(FLAG_BADGE05_GET, battlerIdDef))
-        defense = (110 * defense) / 100;
+        defense = (100 * defense) / 100;
     if (ShouldGetStatBadgeBoost(FLAG_BADGE07_GET, battlerIdAtk))
-        spAttack = (110 * spAttack) / 100;
+        spAttack = (100 * spAttack) / 100;
     if (ShouldGetStatBadgeBoost(FLAG_BADGE07_GET, battlerIdDef))
-        spDefense = (110 * spDefense) / 100;
+        spDefense = (100 * spDefense) / 100;
 
     // Apply type-bonus hold item
     for (i = 0; i < ARRAY_COUNT(sHoldEffectToType); i++)
