5264b537a0cb471b64a586e52c27c1618865520a
user-dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-09-08 12:12:26 -0300

    Item Hack (Focus Band)

--- src/battle_script_commands.c
+++ src/battle_script_commands.c
@@ -1818,7 +1818,7 @@ static void Cmd_adjustnormaldamage(void)
 
     gPotentialItemEffectBattler = gBattlerTarget;
 
-    if (holdEffect == HOLD_EFFECT_FOCUS_BAND && (Random() % 100) < param)
+    if (holdEffect == HOLD_EFFECT_FOCUS_BAND && (gDisableStructs[gBattlerTarget].isFirstTurn == 2 || gBattleMons[gBattlerTarget].hp == gBattleMons[gBattlerTarget].maxHP || (Random() % 100) < param))
     {
         RecordItemEffectBattle(gBattlerTarget, holdEffect);
         gSpecialStatuses[gBattlerTarget].focusBanded = 1;
@@ -1879,7 +1879,7 @@ static void Cmd_adjustnormaldamage2(void)
 
     gPotentialItemEffectBattler = gBattlerTarget;
 
-    if (holdEffect == HOLD_EFFECT_FOCUS_BAND && (Random() % 100) < param)
+    if (holdEffect == HOLD_EFFECT_FOCUS_BAND && (gDisableStructs[gBattlerTarget].isFirstTurn == 2 || gBattleMons[gBattlerTarget].hp == gBattleMons[gBattlerTarget].maxHP || (Random() % 100) < param))
     {
         RecordItemEffectBattle(gBattlerTarget, holdEffect);
         gSpecialStatuses[gBattlerTarget].focusBanded = 1;
@@ -5980,7 +5980,7 @@ static void Cmd_adjustsetdamage(void)
 
     gPotentialItemEffectBattler = gBattlerTarget;
 
-    if (holdEffect == HOLD_EFFECT_FOCUS_BAND && (Random() % 100) < param)
+    if (holdEffect == HOLD_EFFECT_FOCUS_BAND && (gDisableStructs[gBattlerTarget].isFirstTurn == 2 || gBattleMons[gBattlerTarget].hp == gBattleMons[gBattlerTarget].maxHP || (Random() % 100) < param))
     {
         RecordItemEffectBattle(gBattlerTarget, holdEffect);
         gSpecialStatuses[gBattlerTarget].focusBanded = 1;
@@ -7515,7 +7515,7 @@ static void Cmd_tryKO(void)
 
     gPotentialItemEffectBattler = gBattlerTarget;
 
-    if (holdEffect == HOLD_EFFECT_FOCUS_BAND && (Random() % 100) < param)
+    if (holdEffect == HOLD_EFFECT_FOCUS_BAND && (gDisableStructs[gBattlerTarget].isFirstTurn == 2 || gBattleMons[gBattlerTarget].hp == gBattleMons[gBattlerTarget].maxHP || (Random() % 100) < param))
     {
         RecordItemEffectBattle(gBattlerTarget, HOLD_EFFECT_FOCUS_BAND);
         gSpecialStatuses[gBattlerTarget].focusBanded = 1;
