9b9c994b657d59bd8c91af35e5e12ee5aefa9a9f
user-dreamaker <135664566+user-dreamaker@users.noreply.github.com>
2026-09-25 15:31:30 -0300

    Ghost Marowak

--- src/battle_setup.c
+++ src/battle_setup.c
@@ -323,7 +323,7 @@ void StartMarowakBattle(void)
     if (CheckBagHasItem(ITEM_SILPH_SCOPE, 1))
     {
         gBattleTypeFlags = BATTLE_TYPE_GHOST | BATTLE_TYPE_GHOST_UNVEILED;
-        CreateMonWithGenderNatureLetter(gEnemyParty, SPECIES_MAROWAK, 30, 31, MON_FEMALE, NATURE_SERIOUS, 0);
+        CreateMonWithGenderNatureLetter(gEnemyParty, SPECIES_MAROWAK, 33, 31, MON_FEMALE, NATURE_ADAMANT, 0);
     }
     else
     {
